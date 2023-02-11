148827923df12cf359876c3d5a5c15904bc60445
Aksel Dindisyan <aksel.dindisyan@sabanciuniv.edu>
2023-02-11 17:06:58 +0300

    Flutter projesini açtım

---
+++ lib/main.dart
@@ -0,0 +1,12 @@
+import 'package:flutter/material.dart';
+
+void main() {
+  // ignore: prefer_const_constructors
+  runApp(MaterialApp(
+    home: Scaffold(
+      appBar: AppBar(
+        title: Text("Mutlu Bayramlar"),
+      ),
+    ),
+  ));
+}
