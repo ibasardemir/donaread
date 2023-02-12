37818793e239a4c3ab653f402221bf84d55e2dc1
Aksel Dindisyan <aksel.dindisyan@sabanciuniv.edu>
2023-02-12 20:41:01 +0300

    Auth

--- lib/main.dart
+++ lib/main.dart
@@ -1,12 +1,24 @@
+import 'package:sampleproject/widget_tree.dart';
+import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
 
-void main() {
-  // ignore: prefer_const_constructors
-  runApp(MaterialApp(
-    home: Scaffold(
-      appBar: AppBar(
-        title: Text("Mutlu Bayramlar"),
+Future<void> main() async {
+  WidgetsFlutterBinding.ensureInitialized();
+  await Firebase.initializeApp();
+  runApp(const MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      debugShowCheckedModeBanner: false,
+      theme: ThemeData(
+        primarySwatch: Colors.orange,
       ),
-    ),
-  ));
+      home: const WidgetTree(),
+    );
+  }
 }
