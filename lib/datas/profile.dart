be2e9aecd05e01612b85321c4506df5a7e182b5b
alenhrantoglu <124913981+alenhrantoglu@users.noreply.github.com>
2023-03-30 23:11:50 +0300

    Update profile.dart

--- lib/datas/profile.dart
+++ lib/datas/profile.dart
@@ -1,37 +1,43 @@
 class Profile {
   late String uid;
   late String name;
+  late String surname;
+  late String phoneNumber;
   late String location;
   int token = 1;
   int booksdonated = 0;
   int booksshared = 0;
   int bookstaken = 0;
   //constructor
-  Profile(this.name, this.uid,  this.location){
+  Profile(this.name, this.surname, this.phoneNumber, this.uid, this.location) {
     //to be written
   }
-  Profile.fromweb(Map user){
-    uid=user["uid"];
+  Profile.fromweb(Map user) {
+    surname = user["surname"];
+    phoneNumber = user["phoneNumber"];
+    uid = user["uid"];
     name = user["name"];
-    location=user["location"];
+    location = user["location"];
     token = user["token"] ?? 1;
-    booksdonated = user["booksdonated"]??0;
-    booksshared = user["booksshared"]??0;
-    bookstaken = user["bookstaken"]??0;
-
+    booksdonated = user["booksdonated"] ?? 0;
+    booksshared = user["booksshared"] ?? 0;
+    bookstaken = user["bookstaken"] ?? 0;
   }
-  Map upload(){
-    Map map ={};
-    map["uid"]=uid;
-    map["name"]=name;
-    map["location"]=location;
-    map["booksdonated"]=booksdonated;
-    map["booksshared"]= booksshared;
-    map["bookstaken"]=bookstaken;
+  Map upload() {
+    Map map = {};
+    map["uid"] = uid;
+    map["surname"] = surname;
+    map["phoneNumber"] = phoneNumber;
+    map["name"] = name;
+    map["location"] = location;
+    map["booksdonated"] = booksdonated;
+    map["booksshared"] = booksshared;
+    map["bookstaken"] = bookstaken;
 
     return map;
   }
-  String adress(){
+
+  String adress() {
     return "profiles/$uid";
   }
 }
