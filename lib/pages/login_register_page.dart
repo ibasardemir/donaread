6312c196eb77fa6d2dcfdc87e88cec7820813b12
alenhrantoglu <124913981+alenhrantoglu@users.noreply.github.com>
2023-03-30 20:32:01 +0300

    Update login_register_page.dart

--- lib/pages/login_register_page.dart
+++ lib/pages/login_register_page.dart
@@ -37,7 +37,10 @@ class LoginPageState extends State<LoginPage> {
         email: _controllerEmail.text,
         password: _controllerPassword.text,
       );
-      //Profilemanager.register(Profile("basar",Auth().currentUser?.uid??"","ist"));
+      Navigator.push(
+        context,
+        MaterialPageRoute(builder: (context) => AdditionalUserInfoPage()),
+      );
     } on FirebaseException catch (e) {
       setState(() {
         errorMessage = e.message;
@@ -107,6 +110,57 @@ class LoginPageState extends State<LoginPage> {
               _submitButton(),
               _loginOrRegistrationButton(),
             ]),
+      ),
+    );
+  }
+}
+
+class AdditionalUserInfoPage extends StatefulWidget {
+  const AdditionalUserInfoPage({Key? key}) : super(key: key);
+
+  @override
+  _AdditionalUserInfoPageState createState() => _AdditionalUserInfoPageState();
+}
+
+class _AdditionalUserInfoPageState extends State<AdditionalUserInfoPage> {
+  final TextEditingController _controllerName = TextEditingController();
+  final TextEditingController _controllerSurname = TextEditingController();
+  final TextEditingController _controllerPhoneNumber = TextEditingController();
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        shadowColor: Colors.green,
+        backgroundColor: Colors.green,
+        title: Text("Register"),
+      ),
+      body: Container(
+        height: double.infinity,
+        width: double.infinity,
+        padding: const EdgeInsets.all(20),
+        child: Column(
+          crossAxisAlignment: CrossAxisAlignment.center,
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: <Widget>[
+            _entryField("Name", _controllerName),
+            _entryField("Surname", _controllerSurname),
+            _entryField("Phone Number", _controllerPhoneNumber),
+            ElevatedButton(
+              onPressed: () {},
+              child: Text("Save"),
+            ),
+          ],
+        ),
+      ),
+    );
+  }
+
+  Widget _entryField(String title, TextEditingController controller) {
+    return TextField(
+      controller: controller,
+      decoration: InputDecoration(
+        labelText: title,
       ),
     );
   }
