36ac1dfe382bcd3dd9e2c4c55e47fd1d93cdad00
alenhrantoglu <124913981+alenhrantoglu@users.noreply.github.com>
2023-04-01 16:51:08 +0300

    Update additionaluserinfo.dart

--- lib/pages/additionaluserinfo.dart
+++ lib/pages/additionaluserinfo.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+
 class AdditionalUserInfoPage extends StatefulWidget {
   const AdditionalUserInfoPage({Key? key}) : super(key: key);
 
@@ -7,6 +8,7 @@ class AdditionalUserInfoPage extends StatefulWidget {
 }
 
 class _AdditionalUserInfoPageState extends State<AdditionalUserInfoPage> {
+  // Text editing controllers for the three input fields
   final TextEditingController _controllerName = TextEditingController();
   final TextEditingController _controllerSurname = TextEditingController();
   final TextEditingController _controllerPhoneNumber = TextEditingController();
@@ -20,16 +22,22 @@ class _AdditionalUserInfoPageState extends State<AdditionalUserInfoPage> {
         title: const Text("Register"),
       ),
       body: Container(
+        // Fill the entire height and width of the screen
         height: double.infinity,
         width: double.infinity,
         padding: const EdgeInsets.all(20),
         child: Column(
+          // Center the contents horizontally and vertically
           crossAxisAlignment: CrossAxisAlignment.center,
           mainAxisAlignment: MainAxisAlignment.center,
           children: <Widget>[
+            // Create a text input field for the user's name
             _entryField("Name", _controllerName),
+            // Create a text input field for the user's surname
             _entryField("Surname", _controllerSurname),
+            // Create a text input field for the user's phone number
             _entryField("Phone Number", _controllerPhoneNumber),
+            // Create a button to save the user's information (not yet implemented)
             ElevatedButton(
               onPressed: () {},
               child: const Text("Save"),
@@ -40,6 +48,7 @@ class _AdditionalUserInfoPageState extends State<AdditionalUserInfoPage> {
     );
   }
 
+  // Helper function to create a text input field with a label
   Widget _entryField(String title, TextEditingController controller) {
     return TextField(
       controller: controller,
@@ -49,3 +58,4 @@ class _AdditionalUserInfoPageState extends State<AdditionalUserInfoPage> {
     );
   }
 }
+
